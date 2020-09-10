55cf2df4a59173d0db74d4f2748aff42759c253c
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-09-10 10:32:35 -0500

    formatting

--- TEALS-Chapter1-Exercise11/src/com/hervey/app/TwoRockets.java
+++ TEALS-Chapter1-Exercise11/src/com/hervey/app/TwoRockets.java
@@ -1,13 +1,12 @@
 package com.hervey.app;
 
 public class TwoRockets {
-	
+
 	public static void main(String[] args) {
 		TwoRockets.drawTwoRockets();
-		
+
 	}
 
-	
 	private static void drawTwoRockets() {
 		System.out.println("Drawing Two rockets");
 		TwoRockets.drawTheCones();
@@ -15,67 +14,60 @@ public class TwoRockets {
 		TwoRockets.drawTheLabels();
 		TwoRockets.drawTheBoxes();
 		TwoRockets.drawTheCones();
-		
 	}
-
 
 	private static void drawTheLabels() {
-		//Draw United twice, then CR
+		// Draw United twice, then CR
 		System.out.print("|United|");
 		System.out.print("|United|");
 		System.out.print("\n");
-		//Draw States twice, then CR
+		// Draw States twice, then CR
 		System.out.print("|States|");
 		System.out.print("|States|");
 		System.out.print("\n");
-		
-	}
 
+	}
 
 	private static void drawTheBoxes() {
 		// TODO Auto-generated method stub
-		//draw the top/bottom twice, then CR
+		// draw the top/bottom twice, then CR
 		System.out.print("+------+");
 		System.out.print("+------+");
 		System.out.print("\n");
-		
-		//draw filler twice, then CR
-		System.out.print("|      |");
-		System.out.print("|      |");
-		System.out.print("\n");
-		
-		//draw filler twice, then CR
-		System.out.print("|      |");
-		System.out.print("|      |");
-		System.out.print("\n");
-		
-		
-		//draw the top/bottom twice, then CR
-		System.out.print("+------+");
-		System.out.print("+------+");
-		System.out.print("\n");
-		
-	}
 
+		// draw filler twice, then CR
+		System.out.print("|      |");
+		System.out.print("|      |");
+		System.out.print("\n");
+
+		// draw filler twice, then CR
+		System.out.print("|      |");
+		System.out.print("|      |");
+		System.out.print("\n");
+
+		// draw the top/bottom twice, then CR
+		System.out.print("+------+");
+		System.out.print("+------+");
+		System.out.print("\n");
+
+	}
 
 	private static void drawTheCones() {
-		//draw Cone level2 twice, then CR
+		// draw Cone level2 twice, then CR
 		System.out.print("   /\\   ");
 		System.out.print("   /\\   ");
 		System.out.print("\n");
-		
-		//draw Cone level2 twice, then CR
+
+		// draw Cone level2 twice, then CR
 		System.out.print("  /  \\  ");
 		System.out.print("  /  \\  ");
 		System.out.print("\n");
-		
-		//draw Cone level1 twice, then CR
+
+		// draw Cone level1 twice, then CR
 		System.out.print(" /    \\ ");
 		System.out.print(" /    \\ ");
 		System.out.print("\n");
-		
-		
+
 	}
-	
 
 }
